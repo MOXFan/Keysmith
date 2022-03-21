eb64d734dd72544cce102de55d556597c88e90f1
Doug Streifel <dougstreifel@gmail.com>
2022-03-21 12:37:11 -0700

    Removed MainPage from template and added placeholder views.

---
+++ Keysmith/Views/StandardPinningView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Keysmith.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class StandardPinningView : ContentPage
+    {
+        public StandardPinningView()
+        {
+            InitializeComponent();
+        }
+    }
+}
